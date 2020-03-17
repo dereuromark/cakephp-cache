e4bd1ea2f42bb4956ed3d8abbe91ee671fe75f25
mscherer <mark.scherer@spryker.com>
2020-03-18 00:15:49 +0100

    Disable hooks.

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,4 +5,20 @@ namespace Cache;
 use Cake\Core\BasePlugin;
 
 class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $bootstrapEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $routesEnabled = false;
+
 }
